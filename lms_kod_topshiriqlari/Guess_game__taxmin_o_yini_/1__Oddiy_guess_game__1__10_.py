f6ed22f92a590a2e2ab19decd92a28be119efa4c
ortikaliyevbahodir-vvv <ortikaliyevbahodir@gmail.com>
2026-05-28 15:51:36 +0500

    Add: 1) Oddiy guess game (1..10) [ortikaliyevbahodir-vvv]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/1__Oddiy_guess_game__1__10_.py
@@ -0,0 +1,11 @@
+yashirin_son = 7
+while True:
+    
+    son = int(input())
+    if son < yashirin_son:
+        print("Low")
+    elif son > yashirin_son:
+        print("High")
+    else:
+        print("Correct")
+        break
