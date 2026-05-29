d6ff190cd2eceb14a36655652f72409586a3c1f8
ortikaliyevbahodir-vvv <ortikaliyevbahodir@gmail.com>
2026-05-29 21:54:32 +0500

    Add: 9) 1..n ichida 5 ta son chiqargach to‘xtash (break) [ortikaliyevbahodir-vvv]

---
+++ lms_kod_topshiriqlari/while_bilan_break_va_continue/9__1__n_ichida_5_ta_son_chiqargach_to_xtash__break.py
@@ -0,0 +1,11 @@
+n = int(input())
+if n < 5:
+    count = 1
+    while count <= n:
+        print(count)
+        count += 1
+else:
+    count = 1
+    while count <= 5:
+        print(count)
+        count += 1
