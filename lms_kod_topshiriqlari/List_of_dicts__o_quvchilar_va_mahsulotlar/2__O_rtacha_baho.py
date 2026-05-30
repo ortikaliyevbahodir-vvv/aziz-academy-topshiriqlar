880258ab56511bd1e1146e7fa64b9f39be28186c
ortikaliyevbahodir-vvv <ortikaliyevbahodir@gmail.com>
2026-05-30 15:08:58 +0500

    Add: 2) O‘rtacha baho [ortikaliyevbahodir-vvv]

---
+++ lms_kod_topshiriqlari/List_of_dicts__o_quvchilar_va_mahsulotlar/2__O_rtacha_baho.py
@@ -0,0 +1,8 @@
+n = int(input())
+students = []
+for _ in range(n):
+    name, score = input().split()
+    students.append({'name': name, 'score': int(score)})
+total = sum(student['score'] for student in students)
+average = total / n
+print(float(average))
